96aea49b8d78ba4d9f4f542109dcf7b7b4672093
gresh113 <40521807+gresh113@users.noreply.github.com>
2020-05-12 01:19:54 -0400

    Update KanohiPedestalTileEntity.java

--- src/main/java/com/github/gresh113/bionimine/objects/tileentity/KanohiPedestalTileEntity.java
+++ src/main/java/com/github/gresh113/bionimine/objects/tileentity/KanohiPedestalTileEntity.java
@@ -2,10 +2,16 @@ package com.github.gresh113.bionimine.objects.tileentity;
 
 import com.github.gresh113.bionimine.init.BionimineTileEntityTypes;
 
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.LockableLootTileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
-public class KanohiPedestalTileEntity extends TileEntity{
+public class KanohiPedestalTileEntity extends LockableLootTileEntity{
 	
 	//public int x,y,z;
 	//boolean initialized = false;
@@ -16,6 +22,34 @@ public class KanohiPedestalTileEntity extends TileEntity{
 	
 	public KanohiPedestalTileEntity() {
 		this(BionimineTileEntityTypes.KANOHI_PEDESTAL.get());
+	}
+
+	@Override
+	public int getSizeInventory() {
+		return 1;
+	}
+
+	@Override
+	protected NonNullList<ItemStack> getItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void setItems(NonNullList<ItemStack> itemsIn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected ITextComponent getDefaultName() {
+		return new TranslationTextComponent("kanohi_pedestal");
+	}
+
+	@Override
+	protected Container createMenu(int id, PlayerInventory player) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
