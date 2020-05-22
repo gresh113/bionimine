eb9f0fbba323d49a227175c1a00ce5d52cf3f041
gresh113 <40521807+gresh113@users.noreply.github.com>
2020-05-21 20:15:48 -0400

    Added Kakama

---
+++ src/main/resources/assets/bionimine/textures/item/masks/great_kakama/copy.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\Public\Documents\My Documents\Modding\bioinimine\src\main\resources\assets\bionimine\textures\item\masks\great_kakama"
+(for %%a in (0 1 2 3 4) do (
+copy great_kakama.png great_kakama_%%a.png
+))
--- src/main/resources/assets/bionimine/textures/item/masks/great_kaukau/copy.bat
+++
@@ -1,4 +0,0 @@
-cd "C:\Users\Public\Documents\My Documents\Modding\bioinimine\src\main\resources\assets\bionimine\textures\item\masks\great_kaukau"
-(for %%a in (0 1 2 3 4) do (
-copy great_kaukau.png great_kaukau_%%a.png
-))
